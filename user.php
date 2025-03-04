fbd81dd5918c0d7bcb2b3eb202e985aa2b304337
Hosein <hosinfarhani98@gmail.com>
2025-03-04 20:35:14 +0330

    add profile

--- user.php
+++ user.php
@@ -5,7 +5,7 @@
     <!-- محدود کردن زوم با دو انگشت در گوشی -->
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
-    <title>هنرستان علامه دوانی - پروفایل کاربر</title>
+    <title>هنرستان علامه دوانی - اخبار</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="all.css">
     <link rel="stylesheet" href="user.css">
@@ -24,7 +24,91 @@
 
     <!-- content start -->
 
-    <h1 style="margin-top: 300px;width: 100%;text-align: center;font-size: 30px;margin-bottom:300px;">User-Page</h1>
+    <div class="container">
+        <div class="container-top">
+            <p id="layout">پروفایل کاربر</p>
+        </div>
+        <div class="container-bottom">
+            <div class="container-right">
+                <img id="user-img" src="images/user.png" alt="">
+                <p>جهت تغییر عکس به مدرسه محل تحصیل مراجعه کنید</p>
+            </div>
+            <div class="container-left">
+                <div id="first-cl-info" class="cl-info">
+                    <h2>
+                        نام
+                    </h2>
+                    <span>:</span>
+                    <p>نامه کاربر</p>
+                </div>
+                <div class="cl-info">
+                    <h2>
+                        نام خانوادگی
+                    </h2>
+                    <span>:</span>
+                    <p>فامیلی کاربر</p>
+                </div>
+                <div class="cl-info">
+                    <h2>
+                        نام پدر
+                    </h2>
+                    <span>:</span>
+                    <p>نام پدر کاربر</p>
+                </div>
+                <div class="cl-info">
+                    <h2>
+                        تاریخ تولد
+                    </h2>
+                    <span>:</span>
+                    <p>1404/1/1</p>
+                </div>
+                <div class="cl-info">
+                    <h2>
+                        کد ملی
+                    </h2>
+                    <span>:</span>
+                    <p>1819991111</p>
+                </div>
+                <div class="cl-info">
+                    <h2>
+                        پایه تحصیلی
+                    </h2>
+                    <span>:</span>
+                    <p>یازدهم</p>
+                </div>
+                <div class="cl-info">
+                    <h2>
+                        رشته
+                    </h2>
+                    <span>:</span>
+                    <p>شبکه</p>
+                </div>
+                <div class="cl-info">
+                    <h2>
+                        شماره تلفن
+                    </h2>
+                    <span>:</span>
+                    <p>09059999999</p>
+                </div>
+                <div class="cl-info">
+                    <h2>
+                        کد پستی
+                    </h2>
+                    <span>:</span>
+                    <p>6314111117</p>
+                </div>
+                <div id="last-cl-info" class="cl-info">
+                    <div id="last-cl-content">
+                        <h2>
+                            محل سکونت
+                        </h2>
+                        <span>:</span>
+                    </div>
+                    <p> خوزستان،شهر آبادان ، بلوار شهدا کارکوب زاده ، کوی بهار ، ایستگاه 10 ، هنرستان علامه دوانی</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- content end -->
 
