da16513a708db79292eaf8504093bc09f98044b8
Hosein <hosinfarhani98@gmail.com>
2026-02-22 01:47:58 +0330

    edit font & add article page - photoes & edit css

--- article.php
+++ article.php
@@ -24,7 +24,133 @@
 
     <!-- content start -->
 
-    <h1 style="margin-top: 300px;width: 100%;text-align: center;font-size: 30px;margin-bottom:300px;">به زودی....</h1>
+    <div class="content-img">
+        <div class="content-img-right">
+            <h1 id="c-h1">مقاله ها</h1>
+            <div></div>
+            <p>در این صفحه میتوانید مقاله های هنرستان را مشاهده کنید.</p>
+        </div>
+        <div class="content-img-left">
+            <img src="images/article.png" alt="">
+        </div>
+    </div>
+    <div class="title-4">
+        <div class="title-h2">
+            <i class="fa fa-circle" aria-hidden="true"></i>
+            <h2> آخرین مقاله ها</h1>
+        </div>
+    </div>
+    <div class="cart-container">
+        <img onclick="c_a_left()" id="c-a-left" src="images/arrow.png" alt="arrow" title="arrow">
+        <div class="test-carts">
+            <div class="t-carts">
+                <a href="" class="t-cart">
+                    <div class="t-c-top">
+                        <img src="images/soon.jpg" alt="">
+                    </div>
+                    <div class="t-c-center">
+                        <h1>به زودی...</h1>
+                        <p>
+                            این بخش درحال تکمیل شدن میباشد و به زودی به سایت اضافه میشود
+                        </p>
+                    </div>
+                    <div class="top-line"></div>
+                    <div class="t-c-bottom">
+                        <div class="tcb-deat">
+                            <span id="span-bt">00:00</span>
+                            <span>-</span>
+                            <p>1405/00/00</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="" class="t-cart">
+                    <div class="t-c-top">
+                        <img src="images/soon.jpg" alt="">
+                    </div>
+                    <div class="t-c-center">
+                        <h1>به زودی...</h1>
+                        <p>
+                            این بخش درحال تکمیل شدن میباشد و به زودی به سایت اضافه میشود
+                        </p>
+                    </div>
+                    <div class="top-line"></div>
+                    <div class="t-c-bottom">
+                        <div class="tcb-deat">
+                            <span id="span-bt">00:00</span>
+                            <span>-</span>
+                            <p>1405/00/00</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="" class="t-cart">
+                    <div class="t-c-top">
+                        <img src="images/soon.jpg" alt="">
+                    </div>
+                    <div class="t-c-center">
+                        <h1>به زودی...</h1>
+                        <p>
+                            این بخش درحال تکمیل شدن میباشد و به زودی به سایت اضافه میشود
+                        </p>
+                    </div>
+                    <div class="top-line"></div>
+                    <div class="t-c-bottom">
+                        <div class="tcb-deat">
+                            <span id="span-bt">00:00</span>
+                            <span>-</span>
+                            <p>1405/00/00</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="" class="t-cart">
+                    <div class="t-c-top">
+                        <img src="images/soon.jpg" alt="">
+                    </div>
+                    <div class="t-c-center">
+                        <h1>به زودی...</h1>
+                        <p>
+                            این بخش درحال تکمیل شدن میباشد و به زودی به سایت اضافه میشود
+                        </p>
+                    </div>
+                    <div class="top-line"></div>
+                    <div class="t-c-bottom">
+                        <div class="tcb-deat">
+                            <span id="span-bt">00:00</span>
+                            <span>-</span>
+                            <p>1405/00/00</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="" class="t-cart">
+                    <div class="t-c-top">
+                        <img src="images/soon.jpg" alt="">
+                    </div>
+                    <div class="t-c-center">
+                        <h1>به زودی...</h1>
+                        <p>
+                            این بخش درحال تکمیل شدن میباشد و به زودی به سایت اضافه میشود
+                        </p>
+                    </div>
+                    <div class="top-line"></div>
+                    <div class="t-c-bottom">
+                        <div class="tcb-deat">
+                            <span id="span-bt">00:00</span>
+                            <span>-</span>
+                            <p>1405/00/00</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="arrows-1">
+            <img onclick="c_a_right()" id="c-a-right-2" src="images/arrow.png" alt="arrow" title="arrow">
+            <img onclick="c_a_left()" id="c-a-left-2" src="images/arrow.png" alt="arrow" title="arrow">
+        </div>
+        <div class="arrows-2">
+            <img onclick="c_a_right_2()" id="c-a-right-2" src="images/arrow.png" alt="arrow" title="arrow">
+            <img onclick="c_a_left_2()" id="c-a-left-2" src="images/arrow.png" alt="arrow" title="arrow">
+        </div>
+        <img onclick="c_a_right()" id="c-a-right" src="images/arrow.png" alt="arrow" title="arrow">
+    </div>
 
     <!-- content end -->
 
